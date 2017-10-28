00fc2d6a79d86a073d71f797691516640e0bf085
Kjetil  Myhre Ims <kjetil@myhre-ims.no>
2017-10-28 13:58:28 +0300

    initla

--- src/main/kotlin/no/java/elasticsearch/domain/SleepingPillSpeaker.kt
+++ src/main/kotlin/no/java/elasticsearch/domain/SleepingPillSpeaker.kt
@@ -1,9 +1,9 @@
 package no.java.elasticsearch.domain
 
-class SleepingPillSpeaker (
+class SleepingPillSpeaker (/*
         "twitter": "@mon_beck",
 "pictureId": "a57209f582d248c99f310aec09957095",
 "pictureUrl": "https://sleepingpill.javazone.no/public/picture/a57209f582d248c99f310aec09957095",
 "name": "Monica Beckwith",
 "bio": "Monica Beckwith is an independent performance consultant and instructor optimizing customer applications and systems 
-)
+*/)
